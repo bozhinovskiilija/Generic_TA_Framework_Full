a03dfa03336d579c4c2a374a3d875ee5e288428b
ibozhinovski <ilija.bozhinovski@endava.com>
2022-11-14 00:05:47 +0100

    CommonString.java commented lines

--- src/main/java/data/CommonString.java
+++ src/main/java/data/CommonString.java
@@ -7,33 +7,33 @@ import java.util.Properties;
 
 public class CommonString {
 
-    private static final String localeFile = "locale_"+ PropertiesUtils.getLocale()+".loc";
-    private static final String localePath = "\\locale\\"+localeFile;
-
-    private static final Properties locale = PropertiesUtils.loadPropertiesFile(localePath);
-
-
-
-    private static String getLocaleString(String title) {
-
-        String result = locale.getProperty(title);
-        Assert.assertNotNull(result,"String'" + title + "' does not exist in file " + localeFile + "!");
-        return result;
-    }
-
-
-    //public static final String SUCCESSFUL_LOGOUT_MESSAGE = "You have been logged out.";
-    //public static final String LOGIN_ERROR_MESSAGE = "Invalid username and password.";
+    // private static final String localeFile = "locale_"+ PropertiesUtils.getLocale()+".loc";
+    // private static final String localePath = "\\locale\\"+localeFile;
+    //
+    // private static final Properties locale = PropertiesUtils.loadPropertiesFile(localePath);
+    //
+    //
+    //
+    // private static String getLocaleString(String title) {
+    //
+    //     String result = locale.getProperty(title);
+    //     Assert.assertNotNull(result,"String'" + title + "' does not exist in file " + localeFile + "!");
+    //     return result;
+    // }
 
 
-    public static String getLogoutSuccessMessage(){
+    public static final String SUCCESSFUL_LOGOUT_MESSAGE = "You have been logged out.";
+    public static final String LOGIN_ERROR_MESSAGE = "Invalid username and password.";
 
-        return getLocaleString("SUCCESSFUL_LOGOUT_MESSAGE");
-    }
 
-    public static String getLoginErrorMessage(){
-
-        return getLocaleString("LOGIN_ERROR_MESSAGE");
-    }
+    // public static String getLogoutSuccessMessage(){
+    //
+    //     return getLocaleString("SUCCESSFUL_LOGOUT_MESSAGE");
+    // }
+    //
+    // public static String getLoginErrorMessage(){
+    //
+    //     return getLocaleString("LOGIN_ERROR_MESSAGE");
+    // }
 
 }
